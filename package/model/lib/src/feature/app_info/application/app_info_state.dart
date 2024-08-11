1a1605feaf281d8b5bd5c9e575ff18ef10ab85e1
salan70 <tetsuo21ad@gmail.com>
2024-08-11 18:28:46 +0900

    refactor: 現在のアプリバージョンを保持する Provider を切り出し :recycle:

--- package/model/lib/src/feature/app_info/application/app_info_state.dart
+++ package/model/lib/src/feature/app_info/application/app_info_state.dart
@@ -8,12 +8,18 @@ import '../infrastructure/app_info_repository.dart';
 
 part 'app_info_state.g.dart';
 
+/// 現在のアプリバージョンを保持する。
+@riverpod
+Future<Version> currentAppVersion(CurrentAppVersionRef ref) async {
+  final packageInfo = await PackageInfo.fromPlatform();
+  return Version.parse(packageInfo.version);
+}
+
 /// アプリのアップデートが必要かどうかを返す。
 @riverpod
 Future<bool> needUpdate(NeedUpdateRef ref) async {
   // アプリのバージョンを取得する。
-  final packageInfo = await PackageInfo.fromPlatform();
-  final userAppVersion = Version.parse(packageInfo.version);
+  final userAppVersion = await ref.watch(currentAppVersionProvider.future);
 
   final appInfoRepository = ref.watch(appInfoRepositoryProvider);
   late Version requiredAppVersion;
