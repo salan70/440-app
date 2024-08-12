9c89e0ecd0fe086f55c4adfac97757269e0e1b8c
salan70 <tetsuo21ad@gmail.com>
2024-08-12 10:57:36 +0900

    chore: drift 関連のパッケージを導入 :wrench:

--- package/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ package/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -22,6 +22,7 @@ import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
+import sqlite3_flutter_libs
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -42,5 +43,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
