c39d24380637345b8a23f47a2c562521825474d6
salan70 <tetsuo21ad@gmail.com>
2024-08-12 09:02:39 +0900

    refactor: typo を修正 :recycle:

--- package/common/lib/src/util/exception/firestore_exception.dart
+++ package/common/lib/src/util/exception/firestore_exception.dart
@@ -3,30 +3,30 @@ import 'app_exception.dart';
 class FirestoreException extends AppException {
   const FirestoreException._([
     super.message = 'firestore exception',
-    FistoreExceptionCode? code,
-  ]) : code = code ?? FistoreExceptionCode.unknown;
+    FirestoreExceptionCode? code,
+  ]) : code = code ?? FirestoreExceptionCode.unknown;
 
   /// データが見つからない
   factory FirestoreException.notFound() => const FirestoreException._(
         'データが見つかりませんでした。',
-        FistoreExceptionCode.notFound,
+        FirestoreExceptionCode.notFound,
       );
 
   /// 不明なエラー
   factory FirestoreException.unknown() => const FirestoreException._(
         'An unknown error has occurred.',
-        FistoreExceptionCode.unknown,
+        FirestoreExceptionCode.unknown,
       );
 
   /// エラーコード
-  final FistoreExceptionCode code;
+  final FirestoreExceptionCode code;
 
   @override
   String toString() => 'FirestoreExceptionCode[${code.name}]: $message';
 }
 
 /// データベース関連の例外のエラーコード
-enum FistoreExceptionCode {
+enum FirestoreExceptionCode {
   /// データが見つからない
   notFound,
 
